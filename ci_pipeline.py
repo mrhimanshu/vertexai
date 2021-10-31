7f6cc017e5b8537083b0f260a1eea50fbadbc8d0
Himanshu <hsaini102@gmail.com>
2021-10-31 14:15:47 +0530

    First version

--- ci_pipeline.py
+++ ci_pipeline.py
@@ -1,32 +1,9 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-from IPython import get_ipython
 
-# %%
-get_ipython().system('gcloud auth login')
-get_ipython().system('gcloud projects list')
-
-
-# %%
-get_ipython().system('gsutil iam ch allUsers:objectViewer gs://vertextesting')
-
-
-# %%
-get_ipython().system('gsutil ls gs://vertextesting')
-
-
-# %%
-get_ipython().system('gcloud config set project vertextesting-330105')
-
-
-# %%
 PROJECT_ID = "vertextesting-330105"
 REGION = "us-central1" #though us-central is cheaper
 PIPELINE_ROOT = "gs://vertextesting/pipeline_root"
 
 
-# %%
 import kfp
 from typing import NamedTuple
 from google.cloud import storage
@@ -53,7 +30,6 @@ from kfp.v2.google import experimental
 from kfp.v2.google.client import AIPlatformClient
 
 
-# %%
 @component(
     packages_to_install = [
         "pandas",
@@ -81,8 +57,6 @@ def get_data(
     train.to_csv(dataset_train.path)
     test.to_csv(dataset_test.path)
 
-
-# %%
 @component(
     packages_to_install = [
         "pandas",
@@ -146,60 +120,6 @@ def train_xgb(
     xgmodel.save_model(model.path + ".bst")
 
 
-# %%
-@component(
-    packages_to_install = [
-        "pandas",
-        "sklearn",
-        "xgboost"
-    ],
-    base_image="python:3.9"
-)
-def eval_model(
-    test_set: Input[Dataset],
-    xgb_model: Input[Model],
-    metrics: Output[ClassificationMetrics],
-    smetrics: Output[Metrics],
-    final_model: Output[Model]
-):
-    from xgboost import XGBClassifier
-    import pandas as pd
-    
-    data = pd.read_csv(test_set.path)
-    model = XGBClassifier()
-    model.load_model(xgb_model.path)
-    
-    score = model.score(
-        data.drop(columns=["target"]),
-        data.target,
-    )
-    
-    from sklearn.metrics import roc_curve
-    y_scores =  model.predict_proba(data.drop(columns=["target"]))[:, 1]
-    fpr, tpr, thresholds = roc_curve(
-         y_true=data.target.to_numpy(), y_score=y_scores, pos_label=True
-    )
-    metrics.log_roc_curve(fpr.tolist(), tpr.tolist(), thresholds.tolist())
-    
-    from sklearn.metrics import confusion_matrix
-    y_pred = model.predict(data.drop(columns=["target"]))
-    
-    metrics.log_confusion_matrix(
-       ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'],
-       confusion_matrix(
-           data.target, y_pred
-       ).tolist(),  # .tolist() to convert np array to list.
-    )
-    
-    xgb_model.metadata["test_score"] = float(score)
-    smetrics.log_metric("score", float(score))
-
-    final_model.metadata["Model"] = "v1"
-    
-    #model.save_model("gs://vertextesting/pipeline_root/model")
-
-
-# %%
 @component(
     packages_to_install=["google-cloud-aiplatform", "joblib", "sklearn"],
     base_image="python:3.9"
@@ -248,8 +168,6 @@ def deploy_model(
     vertex_endpoint.uri = endpoint.resource_name
     vertex_model.uri = deployed_model.resource_name
 
-
-# %%
 @dsl.pipeline(
     # Default pipeline root. You can override it when submitting the pipeline.
     pipeline_root=PIPELINE_ROOT,
@@ -280,85 +198,5 @@ def pipeline(
     #         project=PROJECT_ID,
     #         machine_type="n1-standard-4",
     #     )
-
-compiler.Compiler().compile(pipeline_func=pipeline,
-        package_path='xgb_pipe.json')
-
-
-# %%
-from datetime import datetime
-
-TIMESTAMP = datetime.now().strftime("%Y%m%d%H%M%S")
-
-
-# %%
-run1 = pipeline_jobs.PipelineJob(
-    display_name="xgb_pipeline",
-    template_path="xgb_pipe.json",
-    job_id="xgb-pipeline-{0}".format(TIMESTAMP),
-    # parameter_values={"bq_tab*le": "sara-vertex-demos.beans_demo.small_dataset"},
-    enable_caching=True,
-)
-
-
-# %%
-run1.run()
-
-
-# %%
-
-
-
-# %%
-#project id and then endpoint id
-from google.cloud import aiplatform
-
-endpoint = aiplatform.Endpoint(
-    endpoint_name="projects/880212553809/locations/us-central1/endpoints/3433159087032893440"
-)
-
-
-# %%
-a=[6.2, 2.9, 4.3,1.3]
-
-
-# %%
-response = endpoint.predict([a])
-print('Predicted: ', response.predictions)
-
-
-# %%
-deployed_model_id = endpoint.list_models()
-
-
-# %%
-deployed_model_id
-#endpoint.undeploy(deployed_model_id=deployed_model_id)
-
-
-# %%
-# aiplatform.Model.resource_name
-
-
-# %%
-
-
-
-# %%
-# from kfp.v2.google.client import AIPlatformClient
-
-# api_client = AIPlatformClient(
-#                 project_id=PROJECT_ID,
-#                 region=REGION
-#                 )
-
-# response = api_client.create_run_from_job_spec(
-#     'xgb_pipe.json',
-#     enable_caching= False
-# )
-
-
-# %%
-
-
-
+if __name__ == '__main__':
+    compiler.Compiler().compile(pipeline_func=pipeline, package_path='xgb_pipe.json')
