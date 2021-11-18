7bc2570b03e63041f28465a4b9ca983345eb5766
Himanshu <hsaini102@gmail.com>
2021-11-18 19:37:31 +0530

    Neptune integration

--- ci_pipeline.py
+++ ci_pipeline.py
@@ -1,7 +1,7 @@
 
-PROJECT_ID = "vertextesting-330105"
+PROJECT_ID = "vertextesting-332513"
 REGION = "us-central1" #though us-central is cheaper
-PIPELINE_ROOT = "gs://vertextesting/pipeline_root"
+PIPELINE_ROOT = "gs://irisdatasetartifact/pipeline_root"
 
 
 
